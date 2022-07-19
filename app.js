5d904fc6d8fbaa4f3510f397b9ddf21e81bbec20
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-07-19 23:44:55 +0530

    Add articles Get Route

--- app.js
+++ app.js
@@ -3,34 +3,42 @@ const mongoose = require("mongoose");
 const app = express();
 
 // Use EJS
-app.set("view-engine", "EJS");
+app.set("view engine", "ejs");
 // Body-Parser
 app.use(express.urlencoded({ extended: true }));
 // Set Static folder
 app.use(express.static("public"));
 
-// Calling Main function 
+// Calling Main function
 main().catch((err) => {
   console.log(err);
 });
 
 // Main async function
 async function main() {
-  const url = "mongodb://127.0.0.1/";
+  const url = "mongodb://127.0.0.1:27017/";
   const Path = "wikiDB";
   await mongoose.connect(url + Path);
 
   // Schema of wikiDB
-  const wikiSchema = new mongoose.Schema({
-    title: "String",
-    content: "String",
+  const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String,
   });
 
-  // Model/Connection of wikiDB
-  const Article = new mongoose.model("Article", wikiSchema);
+  // Model/Collection of wikiDB
+  const Article = new mongoose.model("Article", articleSchema);
 
-  app.get("/", (req, res) => {
-    res.redirect("/");
+  app.get("/articles", (req, res) => {
+    Article.find({}, (err, result) => {
+      if (!err) {
+        res.render("articles", {
+          articles: result,
+        });
+      } else {
+        res.send("Error inside articles Route !!" + err);
+      }
+    });
   });
 
   app.listen(3000, () => {
